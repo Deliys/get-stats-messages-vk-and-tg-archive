f208f66a69a4a452e5aeeb90d47fb8553af86d94
Develom <37857488+Deliys@users.noreply.github.com>
2022-05-02 17:45:46 +0700

    Рабочая версия
    рабочий подсчет сообщений вк

--- get_messeges.py
+++ get_messeges.py
@@ -4,19 +4,42 @@ from tqdm import tqdm
 import pymorphy2
 import json
 import threading
+import sqlite3
 #193183153
 
+
+
+filters = ["'",",",".",'[',']','{','}','(',")",
+'«','»',"-",";"
+]
 
 
 clear_lits = [
 "Запись",
 "Файл",
-"Стикер"
-
+"Стикер",
+"удалено"
 ]
 
 
+conn = sqlite3.connect('main.db')
+cur = conn.cursor()
 
+cur.execute("""CREATE TABLE IF NOT EXISTS  word(
+   word TEXT,
+   word_normal_form TEXT
+
+);
+""")
+cur.execute("""CREATE TABLE IF NOT EXISTS word_normal(   
+   word TEXT,
+   match INT
+);
+""")
+cur.execute('DELETE FROM word;',);
+cur.execute('DELETE FROM word_normal;',);
+
+conn.commit()
 
 file_name_list = []
 morph = pymorphy2.MorphAnalyzer()
@@ -37,70 +60,99 @@ def split_mass(lst , n):
 	return a
 
 
-for file_dir in os.listdir("Archive/messages"):
-	for file_dir_user in os.listdir("Archive/messages/"+str(file_dir)):
-		file_name_list.append("Archive/messages/"+str(file_dir)+"/"+file_dir_user)
+
+def anti_simfol(text):
+
+	for uu in filters:
+		text = text.translate({ord(i): None for i in uu})
+
+	return text
 
 
-def get_stat_tread(file_name_list,text_prog):
-	data = {}
-	for file_name in tqdm(file_name_list ,desc=("#"+str(text_prog))):
+
+#получение всех сообщений из переписок вк
+
+def get_vk_messages_list():
+	data = []
+	list_dir_mess = []
+	for file_dir in os.listdir("Archive/messages"):
+		if ("index-messages.html" !=file_dir) ==True:
+			if int(file_dir) >0:#проверка на бота
+				for file_dir_user in os.listdir("Archive/messages/"+str(file_dir)):
+						file_name = "Archive/messages/"+str(file_dir)+"/"+file_dir_user
+						list_dir_mess.append(file_name)
+
+	for file_name in tqdm(list_dir_mess):
 		with open(file_name ,"r") as file:
 			f = file.read()
-			soup = BeautifulSoup(f, 'lxml')	
+			soup = BeautifulSoup(f, 'lxml') 
 			messeges = soup.find_all("div", attrs={ "class" : "item"})
+
 			for i in messeges:
 				text = i.find_all("div", attrs={})[3].text
-				if len(text.split())>0:
+				if ((len(text.split())>0) and (text!= " ")):
 					if (text.split()[0] in clear_lits) == False:
-						for ii in text.lower().split():
-							if (ii in data)== True:
-								data[morph.parse(ii)[0]]["mass"] = data[ii]["mass"] + 1
-								data[morph.parse(ii)[0]]["form"].append(ii)
-							else:
-								data[morph.parse(ii)[0]] = {}
-								data[morph.parse(ii)[0]]["mass"] = 1
-								data[morph.parse(ii)[0]]["form"] = [ii]
-			
-
-	with open(str(text_prog)+".json", "w",encoding='utf-8') as file:
-		json.dump(data, file, indent=4, ensure_ascii=False)
+						data.append(text)
 
 
+	return data
 
+# просто разбивает элементы массива(сообщения целиком) на слова и создает из них текс
+def list_to_text(lst):
+	text = ""
+	for i in lst:
+		for ii in i.split():
+			text = text + "\n"+ii
 
-# thr1 = threading.Thread(target = get_stat_tread, args = (split_mass(file_name_list,4)[0],))
-# thr2 = threading.Thread(target = get_stat_tread, args = (split_mass(file_name_list,4)[1],))
-# thr3 = threading.Thread(target = get_stat_tread, args = (split_mass(file_name_list,4)[2],))
-# thr4 = threading.Thread(target = get_stat_tread, args = (split_mass(file_name_list,4)[3],))
+	return text
 
-#колво потоков 
-tread_mathc = 4
+#  просто разбивает элементы массива(сообщения целиком) на слова и переделывает в общую форму
+def list_to_normal_form(lst):
 
-
-mass = [] #тут хранятся потоки
-file_name_list = split_mass(file_name_list,tread_mathc)
-for i in range(0,tread_mathc):
-	mass.append(threading.Thread(target = get_stat_tread, args = (file_name_list[i], i,)))
-for i in mass:
-	i.start()
-
-for i in mass:
-	i.join()
-
-
-
-# for i in data:
-# 	print(i , data[i]["mass"])
+	text_end = []
+	for i in lst:
+		for ii in i.split():
+			text_end.append(morph.parse(ii)[0].normal_form)
+			#print(ii , morph.parse(ii)[0].normal_form)
+			#input()
+	
+	return text_end
 
 
 
 
+tesst = get_vk_messages_list()
+
+text = []
+
+for i in tesst:
+	for ii in i.split():
+		text.append(ii)
 
 
-# def myfunc(a, b): 
-#     print('сумма :',a + b) 
-# thr1 = threading.Thread(target = myfunc, args = (1, 2))
-# thr1.start()
+cur = conn.cursor()
+for i in tqdm(text):
+	i= anti_simfol(i)
+	cur.execute("INSERT INTO word VALUES(?,?);",(i.lower() ,morph.parse(i)[0].normal_form,) )	
+conn.commit()
+
+
+
+cur = conn.cursor()
+print(cur.execute("""INSERT INTO word_normal(word,match) SELECT
+	`word_normal_form`,
+	COUNT(`word_normal_form`) AS `count`
+FROM
+	`word`
+GROUP BY
+	`word_normal_form`
+HAVING 
+	`count` > 1"""))
+conn.commit()
+
+
+
+
 
 input("готово")
+
